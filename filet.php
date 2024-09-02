db1b0da74f292899c5aecc64fb8c2c2c7fe859a5
Kastriottm <kastriot@bareshamusic.com>
2024-09-02 21:15:16 +0200

    #

--- filet.php
+++ filet.php
@@ -1,21 +1,18 @@
 <?php include 'partials/header.php';
 if (isset($_POST['ruaj'])) {
   $pershkrimi = mysqli_real_escape_string($conn, $_POST['pershkrimi']);
-
   // File upload handling
   $targetfolder = "dokument/";
   $filename = basename($_FILES['fileToUpload']['name']);
   $targetfolder = $targetfolder . $filename;
   $file_type = $_FILES['fileToUpload']['type'];
   $uploadOk = false;
-
   // Check file type and move the file
   if ($file_type == "application/pdf") {
     if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $targetfolder)) {
       $uploadOk = true;
     }
   }
-
   // Database insertion if the file was uploaded successfully
   if ($uploadOk) {
     if ($conn->query("INSERT INTO filet (pershkrimi, file) VALUES ('$pershkrimi', '$targetfolder')")) {
@@ -28,47 +25,53 @@ if (isset($_POST['ruaj'])) {
   }
 }
 ?>
-
 <div class="main-panel">
   <div class="content-wrapper">
     <div class="container-fluid">
       <div class="container">
-        <div class="p-5 rounded-5 shadow-sm mb-4 card">
-          <h4 class="font-weight-bold text-gray-800 mb-4 text-dark">Dokumente tjera</h4>
-          <nav class="d-flex">
-            <h6 class="mb-0">
-              <a href="filet.php" class="text-reset text-dark" data-bs-placement="top" data-bs-toggle="tooltip" title="<?php echo __FILE__; ?>"><u>Dokumente tjera</u></a>
-            </h6>
-          </nav>
+        <nav class="bg-white px-2 rounded-5" style="width:fit-content;" aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item "><a class="text-reset" style="text-decoration: none;">Dokumente</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+              <a href="<?php echo __FILE__; ?>" class="text-reset" style="text-decoration: none;">
+                Lista e dokumenteve
+              </a>
+            </li>
+          </ol>
+        </nav>
+        <!-- Button trigger modal -->
+        <button type="button" class="input-custom-css px-3 py-2" data-bs-toggle="modal" data-bs-target="#ngarkofile">
+          <i class="fi fi-rr-add"></i>&nbsp; Shto
+        </button>
+        <br> <br>
+        <!-- Modal for File Upload -->
+        <div class="modal fade" id="ngarkofile" name="ngarkofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ngarko nj&euml; file</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form action="filet.php" method="post" enctype="multipart/form-data">
+                  <label>Ngarko nj&euml; file:</label>
+                  <input type="file" name="fileToUpload" id="fileToUpload" class="form-control" required>
+                  <label>P&euml;rshkrimi</label>
+                  <input type="text" name="pershkrimi" placeholder="P&euml;rshkrimi" class="form-control" required>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hiqe</button>
+                <button type="submit" class="btn btn-primary" name="ruaj">Ruaj</button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="card rounded-5 shadow-sm">
           <div class="card-body">
-            <h4 class="card-title">Dokumente t&euml; ndryshme</h4>
             <div class="row">
               <div class="col-12">
-                <!-- Modal for File Upload -->
-                <div class="modal fade" id="ngarkofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Ngarko nj&euml; file</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                        <form action="filet.php" method="post" enctype="multipart/form-data">
-                          <label>Ngarko nj&euml; file:</label>
-                          <input type="file" name="fileToUpload" id="fileToUpload" class="form-control" required>
-                          <label>P&euml;rshkrimi</label>
-                          <input type="text" name="pershkrimi" placeholder="P&euml;rshkrimi" class="form-control" required>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hiqe</button>
-                        <button type="submit" class="btn btn-primary" name="ruaj">Ruaj</button>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
                 <!-- Table -->
                 <div class="table-responsive">
                   <table id="example" class="table w-100 table-bordered">
@@ -88,26 +91,20 @@ if (isset($_POST['ruaj'])) {
                           <td><?php echo $k['pershkrimi']; ?></td>
                           <td>
                             <?php if (file_exists($k['file'])) { ?>
-                              <a class="btn btn-light btn-lg shadow-2 border border-1" href="<?php echo $k['file']; ?>" target="_blank"><i class="fi fi-rr-file-export"></i></a>
+                              <a style="text-decoration: none;" class="input-custom-css px-3 py-2" href="<?php echo $k['file']; ?>" target="_blank"><i class="fi fi-rr-file-export"></i></a>
                             <?php } else { ?>
-                              <button class="btn btn-light btn-lg shadow-2 border border-1" disabled><i class="fi fi-rr-file-export"></i></button>
+                              <button class="input-custom-css px-3 py-2" disabled><i class="fi fi-rr-file-export"></i></button>
                               <div class="text-danger mt-4 p-3 shadow-sm rounded-5 border">Dokumenti p&euml;r p&euml;rshkrimin&nbsp;&nbsp; <i>'<?php echo $k['pershkrimi']; ?>'</i> &nbsp;&nbsp;nuk ekziston.</div>
                             <?php } ?>
                           </td>
                           <td>
-                            <a class="btn btn-light btn-lg shadow-2 border border-1" href="edit_entry.php?id=<?php echo $k['id']; ?>"><i class="fi fi-rr-edit"></i></a>
-                            <a href="api/delete_methods/delete_file.php?id=<?php echo $k['id']; ?>" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this entry?')"><i class="fi fi-rr-trash"></i></a>
+                            <a style="text-decoration: none;" class="input-custom-css px-3 py-2" href="edit_entry.php?id=<?php echo $k['id']; ?>"><i class="fi fi-rr-edit"></i></a>
+                            <a style="text-decoration: none;" href="api/delete_methods/delete_file.php?id=<?php echo $k['id']; ?>" class="input-custom-css px-3 py-2" onclick="return confirm('Are you sure you want to delete this entry?')"><i class="fi fi-rr-trash"></i></a>
                           </td>
                         </tr>
                       <?php } ?>
                     </tbody>
-                    <tfoot class="bg-light">
-                      <tr>
-                        <th class="text-dark">Pershkrimi</th>
-                        <th class="text-dark">Filet</th>
-                        <th></th>
-                      </tr>
-                    </tfoot>
+                   
                   </table>
                 </div>
               </div>
@@ -118,27 +115,23 @@ if (isset($_POST['ruaj'])) {
     </div>
   </div>
 </div>
-
 <?php include 'partials/footer.php'; ?>
-
 <script>
   $('#example').DataTable({
     responsive: true,
     search: {
       return: true,
     },
-    dom: 'Bfrtip',
-    buttons: [{
-      text: '<i class="fi fi-rr-folder-upload fa-lg"></i>&nbsp;&nbsp; Shto nje dokument',
-      className: 'btn btn-light border shadow-2 me-2',
-      action: function(e, node, config) {
-        $('#ngarkofile').modal('show')
-      }
-    }],
     initComplete: function() {
-      var btns = $('.dt-buttons');
-      btns.addClass('');
-      btns.removeClass('dt-buttons btn-group');
+      $(".dt-buttons").removeClass("dt-buttons btn-group");
+      $("div.dataTables_length select").addClass("form-select").css({
+        width: "auto",
+        margin: "0 8px",
+        padding: "0.375rem 1.75rem 0.375rem 0.75rem",
+        lineHeight: "1.5",
+        border: "1px solid #ced4da",
+        borderRadius: "0.25rem"
+      });
     },
     fixedHeader: true,
     language: {
